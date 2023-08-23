177a294eec838fa13c156d47e94b1d459eb5e82e
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-08-23 16:13:34 +1200

    load fonts from CDN

--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
-
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import { App } from "./App.tsx";
+import "./main.css";
 
 createRoot(document.querySelector("#root")!).render(<App />);
